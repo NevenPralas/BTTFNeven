e5ddf892d1066e00be7cbf10bf9931d81027727c
NevenPralas <148341676+NevenPralas@users.noreply.github.com>
2024-06-02 22:15:03 +0200

    nadopuniti sutra skriptu

---
+++ Assets/Scripts/Kraj.cs
@@ -0,0 +1,40 @@
+using BNG;
+using Mirror;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kraj : NetworkBehaviour
+{
+    private GameObject snapPresent;
+    private GameObject totem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        snapPresent = GameObject.FindGameObjectWithTag("SnapPresent");
+        totem = GameObject.FindGameObjectWithTag("Totem");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (snapPresent.GetComponent<SnapZone>().HeldItem == totem.GetComponent<Grabbable>())
+        {
+            Debug.LogError("Poziv");
+            CmdKraj();
+
+        }
+    }
+
+    [Command(requiresAuthority = false)]
+    private void CmdKraj()
+    {
+        RpcKraj();
+    }
+
+    [ClientRpc]
+    private void RpcKraj()
+    {
+        Debug.LogError("Kraj RPC");
+    }
+}
